7f8b70adbe8e0c307c554c23c360cc3ce81409b7
KizimJule <kizimjule@gmail.com>
2022-08-30 11:19:50 +0300

    3task start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+player.on('play', onPlay);
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+_.throttle(func, [(wait = 1000)], [(options = {})]);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+const form = document.querySelector('.feedback-form');
+const formData = {};
 
+function hendleInput(evt) {
+  const { name, value } = evt.target;
+  formData[name] = value;
+
+  const formDataJSON = JSON.stringify(formData);
+  console.log(formDataJSON);
+  localStorage.setItem('formKey', formDataJSON);
+}
+form.addEventListener('input', hendleInput);
